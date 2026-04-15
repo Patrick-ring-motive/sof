1b57dda14673eef4e5485313a7fa89f38d4ef7e8
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-14 21:36:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ function BlobURL(blob) {
     registry.register(blobURL, url);
     return blobURL;
 }
+
+  const encoder = new TextEncoder();
+  const encode = encoder.encode.bind(encoder);
+  const decoder = new TextDecoder();
+  const decode = decoder.decode.bind(decoder);
+  const responseChunks = (res) => {
+    const chunks = [];
+    chunks.stream = res?.clone?.()?.body;
+    chunks.pending = true;
+    chunks.done = (async()=>{
+      try {
+        for await (const chunk of chunks.stream) {
+          chunks.push(chunk);
+        }
+      } catch (e) {
+        chunks.error = e;
+      }
+        pending = false;
+        return chunks;
+    })();
+    return chunks;
+  };
