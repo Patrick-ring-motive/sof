86b6e3982e41ecf285d687fd4803f224a4899feb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-30 15:21:46 -0500

    style: js-beautify formatting (index.js)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const registry = new FinalizationRegistry(url => URL.revokeObjectURL(url));
 
 function BlobURL(blob) {
-    const url = URL.createObjectURL(blob);
-    const revoke = () => URL.revokeObjectURL(url);
-    const blobURL = new String(url);
-    blobURL.revoke = revoke;
-    registry.register(blobURL, url);
-    return blobURL;
+  const url = URL.createObjectURL(blob);
+  const revoke = () => URL.revokeObjectURL(url);
+  const blobURL = new String(url);
+  blobURL.revoke = revoke;
+  registry.register(blobURL, url);
+  return blobURL;
 }
 
 const encoder = new TextEncoder();
@@ -14,34 +14,34 @@ const encode = encoder.encode.bind(encoder);
 const decoder = new TextDecoder();
 const decode = decoder.decode.bind(decoder);
 const responseChunks = (res) => {
-    const chunks = [];
-    chunks.stream = res?.clone?.()?.body;
-    chunks.pending = true;
-    chunks.done = (async () => {
-        let resolver;
-        chunks.next = new Promise(resolve=>{
-            resolver = resolve;
+  const chunks = [];
+  chunks.stream = res?.clone?.()?.body;
+  chunks.pending = true;
+  chunks.done = (async () => {
+    let resolver;
+    chunks.next = new Promise(resolve => {
+      resolver = resolve;
+    });
+    try {
+      for await (const chunk of chunks.stream) {
+        chunks.push(chunk);
+        resolver(chunk);
+        chunks.next = new Promise(resolve => {
+          resolver = resolve;
         });
-        try {
-            for await (const chunk of chunks.stream) {
-                chunks.push(chunk);
-                resolver(chunk);
-                chunks.next = new Promise(resolve=>{
-                    resolver = resolve;
-                });
-            }
-        } catch (e) {
-            chunks.error = e;
-            resolver?.({
-                done:true,
-                value:e
-            });
-        }
-        chunks.pending = false;
-        resolver?.({
-                done:true
-        });
-        return chunks;
-    })();
+      }
+    } catch (e) {
+      chunks.error = e;
+      resolver?.({
+        done: true,
+        value: e
+      });
+    }
+    chunks.pending = false;
+    resolver?.({
+      done: true
+    });
     return chunks;
+  })();
+  return chunks;
 };
